5823931c7f0a31ffc1350ed200eed2c16233db70
USER <rajputshubham615@gmail.com>
2022-12-12 14:19:31 +0530

    added the  VehicleDescriptionApplication service

---
+++ Vehicle_Description/src/main/java/com/niit/jdp/Vehicle_Description/VehicleDescriptionApplication.java
@@ -0,0 +1,13 @@
+package com.niit.jdp.Vehicle_Description;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleDescriptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleDescriptionApplication.class, args);
+	}
+
+}
---
+++ Vehicle_Description/src/test/java/com/niit/jdp/Vehicle_Description/VehicleDescriptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niit.jdp.Vehicle_Description;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleDescriptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
